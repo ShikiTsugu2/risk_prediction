b87cbfa594883b3d6f38ae8476bdc700709f360c
Maxime <132160016+ShikiTsugu2@users.noreply.github.com>
2025-03-05 13:34:31 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pandas as pd
 import joblib
+import matplotlib.pyplot as plt
+import seaborn as sns
 from sklearn.preprocessing import StandardScaler
 
 # Charger le modèle et le scaler
@@ -20,6 +22,22 @@ input_data = {}
 
 for col in feature_names:
     input_data[col] = st.number_input(f"{col}", value=0.0)
+
+# Chargement des données pour analyse visuelle
+data = pd.read_csv('credit_risk_dataset.csv', sep=';')
+
+# Affichage de la distribution des variables
+st.subheader("Répartition des ages des clients")
+fig, ax = plt.subplots(figsize=(8, 5))
+sns.histplot(data['person_age'], kde=True, bins=30, ax=ax)
+st.pyplot(fig)
+
+# Affichage de la heatmap de corrélation
+st.subheader("Corrélation entre les Variables Numériques")
+numeric_data = data.select_dtypes(include=['float64', 'int64'])
+fig, ax = plt.subplots(figsize=(10, 6))
+sns.heatmap(numeric_data.corr(), annot=True, cmap='coolwarm', fmt='.2f', ax=ax)
+st.pyplot(fig)
 
 # Bouton de prédiction
 # Le modèle prédit le risque de crédit du client soit loan_status = 1 (risqué) ou loan_status = 0 (non risqué)
